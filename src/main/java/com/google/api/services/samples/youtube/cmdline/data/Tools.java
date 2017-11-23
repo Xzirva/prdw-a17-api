f962922513340a4065b9aca726a2741496b7c638
Huiling <Huiling@shaohuil-hz-3.etu.utc>
2017-11-23 17:50:30 +0100

    add the method separateTime in the class tool

--- src/main/java/com/google/api/services/samples/youtube/cmdline/data/Tools.java
+++ src/main/java/com/google/api/services/samples/youtube/cmdline/data/Tools.java
@@ -1,8 +1,12 @@
 package com.google.api.services.samples.youtube.cmdline.data;
 
 import static com.sun.javafx.util.Utils.split;
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.*;
 
 public class Tools {
+    
     public static String parseTopicCategory(String topiccategory) {
         String[] tmp = topiccategory.split("/");
         try {
@@ -11,6 +15,24 @@ public class Tools {
             return "NA";
         }
     }
+    
+    
+    public static void separateTime(Timestamp time){
+        //Timestamp publishTime = new Timestamp(video.getSnippet().getPublishedAt().getValue());
+        //convert the time to type long
+        long timelong = time.getTime();
+        Calendar cal = Calendar.getInstance();
+        cal.setTimeInMillis(timelong);
+        int year;
+        int month;
+        int day;
+        year = cal.get(Calendar.YEAR);
+        month = cal.get(Calendar.MONTH);
+        day = cal.get(Calendar.DAY_OF_MONTH);
+        //faut enregistrer les variables locales year, month and day
+    }
+    
+    
     public static void main(String[] args){
         System.out.println(parseTopicCategory("https://en.wikipedia.org/wiki/Society"));
     }
