4c3ec86e15d5e018ca67a095b54deabfa89b3213
louisste <stephane_louis@hotmail.fr>
2017-11-23 17:22:50 +0100

    updated tools with parseTopicCategory

--- src/main/java/com/google/api/services/samples/youtube/cmdline/data/Tools.java
+++ src/main/java/com/google/api/services/samples/youtube/cmdline/data/Tools.java
@@ -1,5 +1,17 @@
 package com.google.api.services.samples.youtube.cmdline.data;
 
+import static com.sun.javafx.util.Utils.split;
+
 public class Tools {
-    
+    public static String parseTopicCategory(String topiccategory) {
+        String[] tmp = topiccategory.split("/");
+        try {
+            return tmp[tmp.length - 1];
+        } catch (Exception e) {
+            return "NA";
+        }
+    }
+    public static void main(String[] args){
+        System.out.println(parseTopicCategory("https://en.wikipedia.org/wiki/Society"));
+    }
 }
