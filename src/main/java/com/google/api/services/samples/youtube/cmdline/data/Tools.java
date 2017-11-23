fe5c3eaa1234ff5c4ec57d4508d5f643d7815bf3
louisste <stephane_louis@hotmail.fr>
2017-11-23 18:11:03 +0100

    added date methods

--- src/main/java/com/google/api/services/samples/youtube/cmdline/data/Tools.java
+++ src/main/java/com/google/api/services/samples/youtube/cmdline/data/Tools.java
@@ -1,5 +1,13 @@
 package com.google.api.services.samples.youtube.cmdline.data;
 
+import com.google.api.client.util.DateTime;
+
+import java.io.*;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import static com.sun.javafx.util.Utils.split;
 
 public class Tools {
@@ -13,5 +21,35 @@ public class Tools {
     }
     public static void main(String[] args){
         System.out.println(parseTopicCategory("https://en.wikipedia.org/wiki/Society"));
+        writeLog();
+        System.out.println(getDateTime().toString());
+    }
+    public static void writeLog(){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yy HH:mm:ss");
+        Date date = new Date();
+        try {
+            BufferedWriter logWriter = new BufferedWriter(new FileWriter(" api.log"));
+            logWriter.write(dateFormat.format(date));
+            logWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static DateTime getDateTime(){
+        try {
+            BufferedReader logReader = new BufferedReader(new FileReader(" api.log"));
+            String readDate = logReader.readLine();
+            SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yy HH:mm:ss");
+            Date tmpDate= sdf.parse(readDate);
+            DateTime googleDate = new DateTime(tmpDate);
+            logReader.close();
+            return googleDate;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
